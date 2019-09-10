74b2d8b1828fbcc9c2f9966dcbd6df6434de9055
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-09-09 22:31:09 -0300

    adicionando modificacoes para salvar e css

--- app1/app.js
+++ app1/app.js
@@ -7,16 +7,39 @@ const endereço = 'https://lab01-projsw-ufcg.herokuapp.com/api/disciplinas';
 function fetch_disciplinas(dados) {
 
     disciplinas = dados;
-    let $disciplinas = document.querySelector("div"); 
+    let $disciplinas = document.querySelector("#disciplinas"); 
     $disciplinas.innerHTML = ''; 
     disciplinas.forEach((e,i) => {
         let $p = document.createElement("p");
         $disciplinas.appendChild($p); 
-        $p.innerText = "nome da disciplina: " + disciplinas[i].nome + ", nota: " + disciplinas[i].nota;  
+        $p.innerText = "Nome: " + disciplinas[i].nome + ", Nota: " + disciplinas[i].nota;  
     }) 
 }
 
+function save_disciplina() {
+
+    let nome = document.querySelector("#nome").value;
+    let nota = document.querySelector("#nota").value; 
+
+    fetch(endereço, {
+        'method' : 'POST',
+        'body': `{"nome": "${nome}", "nota": ${nota}}`,
+        'headers': {'Content-Type': 'application/json'}
+    })
+    .then(r => r.json())
+    .then(d => {
+        disciplinas.push(d);
+        fetch_disciplinas(disciplinas);
+    }); 
+
+    document.querySelector("#nome").value = '';
+    document.querySelector("#nota").value = '';  
+    
+}
+
 (function run() {
+    let $button = document.querySelector("#buttonCadastro");
+    $button.addEventListener('click', save_disciplina);
     fetch(endereço)
     .then(r => r.json())
     .then(fetch_disciplinas)
