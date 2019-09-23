9adec13722d4e2ef3ecf788cf3333e13c40411d1
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-09-23 10:08:08 -0300

    modificacao no jwt service

--- lab02/src/main/java/disciplineControl/services/JwtService.java
+++ lab02/src/main/java/disciplineControl/services/JwtService.java
@@ -1,5 +1,20 @@
 package disciplineControl.services;
 
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
 public class JwtService {
+
+	private final String TOKEN_KEY = "minha_senha";
+
+	public String geraToken(String email) {
+		return Jwts.builder().setSubject(email).signWith(SignatureAlgorithm.HS512, TOKEN_KEY)
+				.setExpiration(new Date(System.currentTimeMillis() + 1 * 60 * 1000)).compact();
+	}
 
 }
